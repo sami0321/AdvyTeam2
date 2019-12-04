a8138eed8b7e9adcf3aca6ccdbe0a3408c332445
Chaouch yassine <yassine.chaouch@esprit.tn>
2019-12-04 22:42:09 +0100

    10 commit

---
+++ Web/Models/Eval360ViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenWeb.Models
+{
+    public class Eval360ViewModels
+    {
+        public int id { get; set; }
+
+        public int? emp_U_ID { get; set; }
+
+        public virtual EmployeeViewModels employee { get; set; }
+    }
+}
