4040a9b9fc9d6bc69df94d75860bbe79feff7804
Chaouch yassine <yassine.chaouch@esprit.tn>
2019-12-05 00:31:26 +0100

    Lastpush

---
+++ Web/Models/EmployeeCommentaireViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenWeb.Models
+{
+    public class EmployeeCommentaireViewModels
+    {
+        public Eval360ViewModels eval360 { get; set; }
+        public List<CommentaireViewModels> commentaire { get; set; }
+    }
+}
